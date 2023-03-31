062602ce270010b6449d1ee7495d0b42d47d2524
AlexandrovaElisabeth <119954939+AlexandrovaElisabeth@users.noreply.github.com>
2023-03-31 15:05:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,28 @@
 // Copyright 2021 NNTU-CS
+
+int cbinsearch(int* arr, int len, int value) {
+    int left = 0, right = len; count = 0;
+    while (right - left > 1) {
+        int mid = (left + right) / 2;
+        if (arr[mid] == value) {
+            int c = 0;
+            while (arr[mid + c] == value) {
+                c++;
+                count++;
+            }
+            c = 1;
+            while (arr[mid - c] == value) {
+                c++;
+                count++;
+            }
+        return count;
+        } else if (arr[mid] < value) {
+            left = mid;
+        } else {right = mid;}
+    }
+    return count;
+}
+
 int countPairs1(int *arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len-1; i++) {
@@ -25,28 +49,6 @@ int countPairs2(int *arr, int len, int value) {
     return count;
 }
 
-int cbinsearch(int* arr, int len, int value) {
-    int left = 0, right = len; count = 0;
-    while (right - left > 1) {
-        int mid = (left + right) / 2;
-        if (arr[mid] == value) {
-            int c = 0;
-            while (arr[mid + c] == value) {
-                c++;
-                count++;
-            }
-            c = 1;
-            while (arr[mid - c] == value) {
-                c++;
-                count++;
-            }
-        return count;
-        } else if (arr[mid] < value) {
-            left = mid;
-        } else {right = mid;}
-    }
-    return count;
-}
 int countPairs3(int* arr, int len, int value) {
     int count = 0;
     for (int i = 0; i < len - 1; i++) {
