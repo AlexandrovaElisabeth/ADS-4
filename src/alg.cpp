559b3686200e011e5c0bf2ce01296ceb16dfaa74
AlexandrovaElisabeth <119954939+AlexandrovaElisabeth@users.noreply.github.com>
2023-03-31 06:03:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,18 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+int count = 0;
+    for (int i = 0; i < len-1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value)
+                count++;
+        }
+    }
+    if (count == 0) {
+        return 0;
+    }
+    return count;
 }
+
 int countPairs2(int *arr, int len, int value) {
   return 0;
 }
